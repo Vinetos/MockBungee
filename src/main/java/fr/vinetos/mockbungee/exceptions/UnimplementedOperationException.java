74a2e3d95de077a199df2a100beee9579623cb83
0ddlyoko <0ddlyokoOfficial@gmail.com>
2021-05-21 13:35:18 +0200

    [Base] Forgot file

---
+++ src/main/java/fr/vinetos/mockbungee/exceptions/UnimplementedOperationException.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2021 Vinetos
+ */
+
+package fr.vinetos.mockbungee.exceptions;
+
+import org.opentest4j.TestAbortedException;
+
+public class UnimplementedOperationException extends TestAbortedException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UnimplementedOperationException() {
+        super("Not implemented");
+    }
+
+    public UnimplementedOperationException(String message) {
+        super(message);
+    }
+}
